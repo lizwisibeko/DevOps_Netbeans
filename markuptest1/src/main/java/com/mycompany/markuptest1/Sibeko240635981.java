444b54c26bab548ea3f0f5229b31bcafcdd2d969
LizwiS <LizwiS@LIZWISLP.MPULEG.GOV.ZA>
2024-05-14 15:01:29 +0200

    My project commit

---
+++ markuptest1/src/main/java/com/mycompany/markuptest1/Sibeko240635981.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.markuptest1;
+
+/**
+ *
+ * @author LizwiS
+ */
+public class Sibeko240635981 {
+   public static void main(String[] args) {
+       int no1=2023;
+       int no2=1;
+        System.out.println("Hello World, This year is = "+no1+no2);
+    }     
+}
